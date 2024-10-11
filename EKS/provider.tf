1d17d3851df75aa8cc261a6376a702aa868a5f00
Amith P S <amithps0@gmail.com>
2024-10-11 21:40:34 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "< 5.71.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-south-1"
+  region  = "ap-south-1"
 }
 
