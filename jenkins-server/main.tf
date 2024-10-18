fc21145f3f30841b135c4f185db7637ccce038fd
amithps <amithps0@gmail.com>
2024-10-19 01:37:59 +0530

    jenkinsconf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -79,3 +79,37 @@ module "ec2_instance" {
     Environment = "dev"
   }
 }
+# an empty resource block
+resource "null_resource" "name" {
+
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/Downloads/awskeypair")
+    host        = aws_instance.ec2_instance.public_ip
+  }
+
+  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+    source      = "install_jenkins.sh"
+    destination = "/tmp/install_jenkins.sh"
+  }
+
+  # set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/install_jenkins.sh",
+      "sh /tmp/install_jenkins.sh",
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = [module.ec2_instance]
+}
+
+
+# print the url of the jenkins server
+output "website_url" {
+  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+}
